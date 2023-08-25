6397de09ff7270d55282ceb06e89788727038bf7
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2023-08-25 15:44:12 -0500

    Crear spiner con paises

--- app/src/main/java/com/example/taller1paises/MainActivity.kt
+++ app/src/main/java/com/example/taller1paises/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.taller1paises
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val arrayAdapter = ArrayAdapter.createFromResource(this, R.array.regiones, android.R.layout.simple_spinner_item)
+        spinner.adapter = arrayAdapter
+
     }
 }
